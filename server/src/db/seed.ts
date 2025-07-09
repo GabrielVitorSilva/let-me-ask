ed1afd203b5c740bf165d1f85c36e45443bbbf26
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-07-09 15:28:56 -0300

    feat: add create question, create room and get room questions and change seed amount

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,21 +1,25 @@
 import { reset, seed } from 'drizzle-seed'
-import { schema } from './schema/index.ts'
 import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
 
 await reset(db, schema)
 
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
+    },
+    questions: {
+      count: 20,
     },
   }
 })
 
 await sql.end()
 
-console.log('Database seeded successfully!')
+// biome-ignore lint/suspicious/noConsole: only used in dev
+console.log('Database seeded')
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,42 @@
-import fastify from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: '*',
+  origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.setErrorHandler((error, _, reply) => {
+  console.error(error)
+
+  reply.status(500).send({
+    message: 'Erro interno do servidor',
+    error: process.env.NODE_ENV === 'development' ? error.message : undefined,
+  })
+})
+
 app.get('/health', () => {
-  return { status: 'ok' }
+  return 'OK'
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Server is running on http://localhost:${env.PORT}`)
-})
+app.listen({ port: env.PORT })
+console.log(`Server is running on http://localhost:${env.PORT}`)
