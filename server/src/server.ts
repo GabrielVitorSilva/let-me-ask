a33591c1b45c9ce6e98f156b7bc08980d9b00a2d
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-07-08 22:10:02 -0300

    feat: atualiza dependências e configurações do ESLint, adiciona suporte a linting no script de desenvolvimento

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,25 @@
-import fastify from "fastify";
-import { 
-  serializerCompiler, 
-  validatorCompiler, 
-  type ZodTypeProvider 
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { fastifyCors} from '@fastify/cors'
-import { env } from "./env.ts";
+import { fastifyCors } from '@fastify/cors'
+import { env } from './env.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: '*',
-}) 
+})
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-  return { status: 'ok' };
+  return { status: 'ok' }
 })
 
-app.listen({port: env.PORT}).then(() => {
-  console.log(`Server is running on http://localhost:${env.PORT}`);
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Server is running on http://localhost:${env.PORT}`)
 })
