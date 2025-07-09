87b7f12357be8a50ab96b64b92b5909f99d9526e
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-07-09 14:15:10 -0300

    feat: instalado biome e tailwindCSS

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,8 +4,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: mandatory by react
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
