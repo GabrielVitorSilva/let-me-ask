c5ded6fd2043a1ec39cd43900c42199ccfa497a2
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-07-09 13:40:07 -0300

    feat: registra a rota getRooms no servidor, permitindo a recuperação de informações sobre as salas

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import {
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,6 +20,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
   return { status: 'ok' }
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server is running on http://localhost:${env.PORT}`)
