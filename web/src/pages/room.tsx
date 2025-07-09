587cf52055efbf6fb9e340a4c09ab881d7e3f9b8
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-07-09 15:07:28 -0300

    feat: atualiza a rota da sala para incluir roomId e implementa a listagem de salas na página de criação, com carregamento assíncrono

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,5 +1,18 @@
+import { Navigate, useParams } from 'react-router-dom'
+
+type RoomParams = {
+  roomId: string
+}
+
 export function Room() {
+  const { roomId } = useParams<RoomParams>()
+
+  if (!roomId) {
+    return <Navigate replace to="/" />
+  }
   return (
-    <div>Room Details</div>
+    <div>
+      <div>Room Details {roomId}</div>
+    </div>
   )
 }
