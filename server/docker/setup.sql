10f6ffb081d54bf9de633542484e974921f60057
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-07-11 17:36:32 -0300

    feat: cria procedimento para extensão vector no banco de dados

--- server/docker/setup.sql
+++ server/docker/setup.sql
@@ -1,1 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS vector;
+CREATE OR REPLACE PROCEDURE create_vector_extension()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'vector') THEN
+        CREATE EXTENSION vector;
+    END IF;
+END;
+$$;
+
+CALL create_vector_extension();
